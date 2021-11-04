0c5f41a7bcfeb6a88338bbd5f774e3652e3a573b
Mysterious-B-Wolf <91982662+Mysterious-B-Wolf@users.noreply.github.com>
2021-11-03 23:05:54 -0400

    Update java.js
    REEE

--- About/java.js
+++ About/java.js
@@ -1,18 +1,18 @@
 	window.addEventListener('resize', function(event){
 		if (window.matchMedia("(max-width: 850px)").matches) {
-    			//hide span with class links
-			document.getElementById("hide").style.visibility = "hidden"
+    			//hide span with id hide
+			document.getElementById("hide").style.visibility = "hidden";
 			//show button with id "show"
 			document.getElementById("show").style.visibility= "visible";
   		} else {
-    			//dont hide span with class links
+    			//dont hide span with id hide
 			document.getElementById("hide").style.visibility= "visible";
-			//hide button with id "show
-			document.getElementById("show").style.visibility = "hidden"
+			//hide button with id "show"
+			document.getElementById("show").style.visibility = "hidden";
 		}
 	});
 function openMenu(){
-	var x = document.getElementById("menu")
+	var x = document.getElementById("menu");
 	if (x.style.display === "block") {
 		x.style.display = "none";
 	} else {
