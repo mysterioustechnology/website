da87cdfbc898d4fbf1022bf6f423c6df179b7783
Mysterious-B-Wolf <91982662+Mysterious-B-Wolf@users.noreply.github.com>
2021-11-04 08:23:03 -0400

    Update java.js

--- About/java.js
+++ About/java.js
@@ -1,17 +1,32 @@
+var hide = document.getElementById("hide")
+var show = document.getElementById("show")
+
 window.addEventListener('resize', function(event) {
     if (window.matchMedia("(max-width: 850px)").matches) {
         //hide span with id hide
-        document.getElementById("hide").style.visibility = "hidden";
+        hide.style.visibility = "hidden";
         //show button with id "show"
-        document.getElementById("show").style.visibility = "visible";
+        show.style.visibility = "visible";
     } else {
         //dont hide span with id hide
-        document.getElementById("hide").style.visibility = "visible";
+        hide.style.visibility = "visible";
         //hide button with id "show"
-        document.getElementById("show").style.visibility = "hidden";
+        show.style.visibility = "hidden";
     }
 });
-
+function onLoad(){
+    if (window.matchMedia("(max-width: 850px)").matches) {
+        //hide span with id hide
+        hide.style.visibility = "hidden";
+        //show button with id "show"
+        show.style.visibility = "visible";
+    } else {
+        //dont hide span with id hide
+        hide.style.visibility = "visible";
+        //hide button with id "show"
+        show.style.visibility = "hidden";
+    }
+}
 function openMenu() {
     var x = document.getElementById("menu");
     if (x.style.display === "block") {
