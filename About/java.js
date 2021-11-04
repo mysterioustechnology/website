555877a95af42945077d4606e4c9a0139d58662f
Mysterious-B-Wolf <91982662+Mysterious-B-Wolf@users.noreply.github.com>
2021-11-03 23:09:13 -0400

    ahhh cleen

--- About/java.js
+++ About/java.js
@@ -1,21 +1,22 @@
-	window.addEventListener('resize', function(event){
-		if (window.matchMedia("(max-width: 850px)").matches) {
-    			//hide span with id hide
-			document.getElementById("hide").style.visibility = "hidden";
-			//show button with id "show"
-			document.getElementById("show").style.visibility= "visible";
-  		} else {
-    			//dont hide span with id hide
-			document.getElementById("hide").style.visibility= "visible";
-			//hide button with id "show"
-			document.getElementById("show").style.visibility = "hidden";
-		}
-	});
-function openMenu(){
-	var x = document.getElementById("menu");
-	if (x.style.display === "block") {
-		x.style.display = "none";
-	} else {
-		x.style.display = "block";
-	}
+window.addEventListener('resize', function(event) {
+    if (window.matchMedia("(max-width: 850px)").matches) {
+        //hide span with id hide
+        document.getElementById("hide").style.visibility = "hidden";
+        //show button with id "show"
+        document.getElementById("show").style.visibility = "visible";
+    } else {
+        //dont hide span with id hide
+        document.getElementById("hide").style.visibility = "visible";
+        //hide button with id "show"
+        document.getElementById("show").style.visibility = "hidden";
+    }
+});
+
+function openMenu() {
+    var x = document.getElementById("menu");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
 }
