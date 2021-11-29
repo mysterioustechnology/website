0b753d7d55ef4f5a71aeb25884f53ffb2b6da4cb
mysterioustechnology <91982662+mysterioustechnology@users.noreply.github.com>
2021-11-29 11:01:00 -0500

    not compleat

--- issues/issues.js
+++ issues/issues.js
@@ -1,17 +1,25 @@
 /*
+passthroue from 404 page
+*/
+function passOver(){
+    var issue =
+
+
+/*
 program for issues form
 */
-
+function onLoad(){
+   var x = document.getElementById('issues_form').style;
+}
 
 //checks if user has github
-colsole.log(
-    var x = document.getElementById('issues_form').style;
-    function check(){
-        if(x.display === 'none'){
-            x.display = 'block';
-        }
-        else{
-            x.display = 'none';
-        }
+
+function check(){
+    if(x.display === 'none'){
+        x.display = 'block';
     }
-)
+    else{
+        x.display = 'none';
+    }
+}
+
