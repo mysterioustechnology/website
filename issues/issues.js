b47eb8c8ee4a814e2d9584cf34cf3eb04100df53
mysterioustechnology <91982662+mysterioustechnology@users.noreply.github.com>
2021-11-29 10:42:55 -0500

    Update issues.js
    aaaa

--- issues/issues.js
+++ issues/issues.js
@@ -2,28 +2,14 @@
 program for issues form
 */
 
-//updates issue box with issue
-function onLoad(issue){
-    document.getElementById('issue_title').innerHTML = issue;
-    console.log(issue)
-}
-
 
 //checks if user has github
-const selectElement = document.querySelector('#has_github');
-var x = false
-selectElement.addEventListener('change', (event) => {
-    if(x){
-        x = false
+var x = document.getElementById('issues_form').style;
+function check(){
+    if(x.display === 'none'){
+        x.display = 'block';
     }
-    else if(!x){
-        x = true;
+    else{
+        x.display = 'none';
     }
-});
-
-if(x){
-    document.open("https://github.com/mysterioustechnology/website/issues")
-}
-else{
-    document.getElementById('issue_form').style = "display:block";
 }
