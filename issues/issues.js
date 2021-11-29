906b1e364609839cdc410ce6be078ea0b9e66216
mysterioustechnology <91982662+mysterioustechnology@users.noreply.github.com>
2021-11-29 10:53:03 -0500

    Update issues.js

--- issues/issues.js
+++ issues/issues.js
@@ -4,12 +4,14 @@ program for issues form
 
 
 //checks if user has github
-var x = document.getElementById('issues_form').style;
-function check(){
-    if(x.display === 'none'){
-        x.display = 'block';
+colsole.log(
+    var x = document.getElementById('issues_form').style;
+    function check(){
+        if(x.display === 'none'){
+            x.display = 'block';
+        }
+        else{
+            x.display = 'none';
+        }
     }
-    else{
-        x.display = 'none';
-    }
-}
+)
